544312ad7abd11f6432cc13a5a6360273a739182
gustavosegantini <70765859+gustavosegantini@users.noreply.github.com>
2023-04-24 19:35:10 -0300

    AtualizaÇão design do cadastro

--- cadastrar_funcionario.php
+++ cadastrar_funcionario.php
@@ -11,21 +11,26 @@
 <body>
     <h1>Cadastrar Funcionário</h1>
     <form id="cadastroFuncionario">
-        <label for="nome">Nome:</label>
-        <input type="text" name="nome" id="nome" required>
-        <br>
-        <label for="email">Email:</label>
-        <input type="email" name="email" id="email" required>
-        <br>
-        <label for="cargo">Cargo:</label>
-        <input type="text" name="cargo" id="cargo" required>
-        <br>
-        <label for="data_admissao">Data de Admissão:</label>
-        <input type="date" name="data_admissao" id="data_admissao" required>
-        <br>
-        <label for="cpf">CPF:</label>
-        <input type="text" name="cpf" id="cpf" minlength="11" maxlength="11" required>
-        <br>
+        <div class="form-group">
+            <label for="nome">Nome:</label>
+            <input type="text" name="nome" id="nome" required>
+        </div>
+        <div class="form-group">
+            <label for="email">Email:</label>
+            <input type="email" name="email" id="email" required>
+        </div>
+        <div class="form-group">
+            <label for="cargo">Cargo:</label>
+            <input type="text" name="cargo" id="cargo" required>
+        </div>
+        <div class="form-group">
+            <label for="data_admissao">Data de Admissão:</label>
+            <input type="date" name="data_admissao" id="data_admissao" required>
+        </div>
+        <div class="form-group">
+            <label for="cpf">CPF:</label>
+            <input type="text" name="cpf" id="cpf" minlength="11" maxlength="11" required>
+        </div>
         <input type="submit" value="Cadastrar Funcionário">
     </form>
     <script src="scripts.js"></script>
