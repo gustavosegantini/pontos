6cf2078b1629c29368b733bd256036a0f0f0320f
gustavosegantini <70765859+gustavosegantini@users.noreply.github.com>
2023-04-27 11:29:50 -0300

    Update registrar_ponto.php

--- registrar_ponto.php
+++ registrar_ponto.php
@@ -1,4 +1,5 @@
 <?php
+date_default_timezone_set('America/Sao_Paulo');
 include '../conexao.php';
 
 $cpf = $_POST['cpf'];
@@ -18,13 +19,13 @@ $ultimoRegistro = $resultUltimoRegistro->fetch_assoc();
 if (!$ultimoRegistro || ($ultimoRegistro['hora_saida'] !== NULL)) {
     $sqlRegistro = "INSERT INTO registro_pontos (funcionario_id, data, hora_entrada) VALUES ('$funcionario_id', '$data', '$hora')";
     $nextEvent = "entrada";
-} elseif ($ultimoRegistro['hora_entrada'] !== NULL && $ultimoRegistro['intervalo_inicio'] === NULL) {
-    $sqlRegistro = "UPDATE registro_pontos SET intervalo_inicio='$hora' WHERE id='{$ultimoRegistro['id']}'";
+} elseif ($ultimoRegistro['hora_entrada'] !== NULL && $ultimoRegistro['intervalo_entrada'] === NULL) {
+    $sqlRegistro = "UPDATE registro_pontos SET intervalo_entrada='$hora' WHERE id='{$ultimoRegistro['id']}'";
     $nextEvent = "início do intervalo";
-} elseif ($ultimoRegistro['intervalo_inicio'] !== NULL && $ultimoRegistro['intervalo_fim'] === NULL) {
-    $sqlRegistro = "UPDATE registro_pontos SET intervalo_fim='$hora' WHERE id='{$ultimoRegistro['id']}'";
+} elseif ($ultimoRegistro['intervalo_entrada'] !== NULL && $ultimoRegistro['intervalo_saida'] === NULL) {
+    $sqlRegistro = "UPDATE registro_pontos SET intervalo_saida='$hora' WHERE id='{$ultimoRegistro['id']}'";
     $nextEvent = "fim do intervalo";
-} elseif ($ultimoRegistro['intervalo_fim'] !== NULL && $ultimoRegistro['hora_saida'] === NULL) {
+} elseif ($ultimoRegistro['intervalo_saida'] !== NULL && $ultimoRegistro['hora_saida'] === NULL) {
     $sqlRegistro = "UPDATE registro_pontos SET hora_saida='$hora' WHERE id='{$ultimoRegistro['id']}'";
     $nextEvent = "saída";
 }
